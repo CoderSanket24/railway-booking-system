cbb3e4a3028f6f7c4c9daf5d172e626f0cbdec7b
Sanket Botre <sanketbotre24@gmail.com>
2026-03-09 16:30:22 +0530

    Initial commit

---
+++ railway-os/src/main/java/com/vit/railway_os/RailwayOsApplication.java
@@ -0,0 +1,13 @@
+package com.vit.railway_os;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayOsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwayOsApplication.class, args);
+	}
+
+}
